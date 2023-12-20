a9536e96158beed3c6971ee178fdff3222e5f471
Tshindimpho <mudzu13@gmail.com>
2023-12-20 23:07:44 +0200

    Implement Dispaly Alll tags ( Get all tags)

--- TechTrendTracker/Controllers/AdminTagsController.cs
+++ TechTrendTracker/Controllers/AdminTagsController.cs
@@ -1,10 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TechTrendTracker.Data;
+using TechTrendTracker.Models.Domain;
 using TechTrendTracker.Models.ViewModels;
 
 namespace TechTrendTracker.Controllers
 {
     public class AdminTagsController : Controller
     {
+
+        private BloggieDbContext bloggieDbContext;
+
+        public AdminTagsController (BloggieDbContext bloggieDbContext)
+        {
+            this.bloggieDbContext = bloggieDbContext;
+        }
 
         [HttpGet]
         public IActionResult Add()
@@ -13,12 +22,38 @@ namespace TechTrendTracker.Controllers
         }
 
         [HttpPost]
-       public IActionResult Add(AddTagRequest  addTagRequest)
+        [ActionName("Add")]
+        public IActionResult Add(AddTagRequest addTagRequest)
         {
-            var name = addTagRequest.Name;
-            var displayName= addTagRequest.DisplayName;
+            //Mapping AddTagRequest to Tag Domain model
+            var tag = new Tag
+            {
+                Name = addTagRequest.Name,
+            DisplayName = addTagRequest.DisplayName
+             };
 
-            return View("Add");
+            bloggieDbContext.Tags.Add(tag);
+            bloggieDbContext.SaveChanges();
+
+            return RedirectToAction("List");
+        }
+
+        [HttpGet]
+        [ActionName("List")]
+        public IActionResult List()
+        {
+            //use dbContext to read the tags
+
+           var tags = bloggieDbContext.Tags.ToList();
+
+            return View(tags);
+        }
+
+        [HttpGet]
+
+        public IActionResult Edit(Guid id)
+        {
+            return View();
         }
     }
 }
